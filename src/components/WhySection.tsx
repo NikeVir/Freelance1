7c1429778e033a3f27bc5346fd7f209bafd77d05
Shivam Agarwal <shivam@synesxi.com>
2024-07-09 00:18:43 +0530

    Made the sections mobile responsive

--- src/components/WhySection.tsx
+++ src/components/WhySection.tsx
@@ -50,15 +50,15 @@ export default function WhySection() {
             <h3 className="text-2xl sm:text-4xl font-semibold tracking-tight text-secondary">
               <span style={{color: '#121118', fontFamily: 'Sora', fontWeight: '400', fontSize: '32px'}}>Cat Proxies</span> <span style={{color: '#121118', fontFamily: 'Sora', fontSize: '32px', fontWeight: '600'}}>Solutions</span>
             </h3>
-            <p className="max-w-xl mx-auto mt-4 text-base text-secondary/80">
-              <span style={{color: '#525252', fontFamily: 'Sora', fontSize: '24px', fontWeight: '400'}}>Explore How Our Expertise Translates</span> <div style={{color: '#525252', fontFamily: 'Sora', fontSize: '24px', fontWeight: '400'}}>into Tangible Results.</div>
+            <p className="flex-col max-w-xl mx-auto mt-4 text-base text-secondary/80">
+              <span style={{color: '#525252', fontFamily: 'Sora', fontSize: '24px', fontWeight: '400'}}>Explore How Our Expertise Translates</span> <span style={{color: '#525252', fontFamily: 'Sora', fontSize: '24px', fontWeight: '400'}}>into Tangible Results.</span>
             </p>
             <p className="text-sm text-gray-400">
               Dive deep into each project with comprehensive case studies that
               outline challenges, strategies, and outcomes.
             </p>
             <div className="sm:flex sm:justify-center lg:justify-start">
-              <img src="/images/illustration/three-cat-illustration.svg" />
+              <img src="/images/illustration/three-cat-illustration.svg" alt="Cat Proxies Illustration" />
             </div>
           </div>
           <div className="mt-10 lg:col-span-7 lg:mt-0">
@@ -96,12 +96,12 @@ export default function WhySection() {
                       </dt>
                       <Disclosure.Panel
                         as="dd"
-                        className="flex flex-col pr-12 mt-8 space-y-4"
+                        className="flex flex-col items-start pr-12 mt-4 space-y-4"
                       >
                         <p className="text-base font-normal text-gray-600">
                           {faq.answer}
                         </p>
-                        <button className="ml-2 px-4 py-4 w-full rounded-lg text-sm text-white bg-gradient-to-r from-[#05C067] to-[#50EA48]">
+                        <button className="px-4 py-2 w-full text-sm text-white rounded-lg bg-gradient-to-r from-[#05C067] to-[#50EA48]">
                           Contact Us Now
                         </button>
                       </Disclosure.Panel>
